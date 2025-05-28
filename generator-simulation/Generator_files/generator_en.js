28f4bbaf5b95d160eef4cef58d147fc5219032e4
Patrick NG <41151294+ngkawaipatrick@users.noreply.github.com>
2025-05-28 14:55:04 +0800

    Add files via upload

---
+++ generator-simulation/Generator_files/generator_en.js
@@ -0,0 +1,26 @@
+// Generator, englische Texte
+// Letzte �nderung 08.12.2017
+
+// Texte in HTML-Schreibweise;
+
+var text01 = "Without commutator";
+var text02 = "With commutator";
+var text03 = "Change rotation direction";
+var text04 = ["Start", "Pause", "Resume"];
+var text05 = "Direction of movement";
+var text06 = "Magnetic field";
+var text07 = "Induced current";
+
+var author = "W. Fendt 1998, Patrick 2025";
+var translator = "";
+
+// Symbole und Einheiten:
+
+var decimalSeparator = ".";                                // Dezimaltrennzeichen (Komma/Punkt)
+var rotationsPerMinute = "rev per min";                        // Umdrehungen pro Minute
+
+// Texte in Unicode-Schreibweise:
+
+var symbolTime = "t";
+var symbolVoltage = "V";
+var symbolResistor = "R";
